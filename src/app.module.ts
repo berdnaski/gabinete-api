42d4bdea317b2af141af42c0a1bedd8070711e86
Berdnaski <berdnaski1177@gmail.com>
2026-03-05 18:40:48 -0300

    chore(app): register new modules and update dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,17 @@ import { DatabaseModule } from './core/database/database.module';
 import { SecurityModule } from './core/security/security.module';
 import { AuthModule } from './core/auth/auth.module';
 
+import { CabinetsModule } from './modules/cabinets/cabinets.module';
+import { UsersModule } from './modules/users/users.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     SecurityModule,
+    CabinetsModule,
     AuthModule,
+    UsersModule,
   ],
 })
 export class AppModule { }
